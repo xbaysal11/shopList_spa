53b9b63399dd78626c819e07de1ae1614cc1f48c
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-03-28 20:04:27 +0600

    MODIFIED style

--- src/App.jsx
+++ src/App.jsx
@@ -14,24 +14,30 @@ class App extends Component {
         <div className="title">
           <img src="https://img.icons8.com/clouds/100/000000/online-shop-shopping-bag.png" />
           <h2>Shopping List</h2>
-        </div>
+          <form>
+            <input type="text" name="product" placeholder="Add New Item" />
 
-        <table className="table-card ">
-          <tr>
-            <th>#</th>
-            <th>Product</th>
-            <th>Action</th>
-          </tr>
-          {buyItem.map(item => {
-            return (
-              <tr>
-                <td>1</td>
-                <td>{item}</td>
-                <td>Button</td>
-              </tr>
-            );
-          })}
-        </table>
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+        <div className="table-card">
+          <table>
+            <tr>
+              <th>#</th>
+              <th>Item</th>
+              <th>Action</th>
+            </tr>
+            {buyItem.map(item => {
+              return (
+                <tr className="item-part">
+                  <td>1</td>
+                  <td>{item}</td>
+                  <td>Button</td>
+                </tr>
+              );
+            })}
+          </table>
+        </div>
       </div>
     );
   }
