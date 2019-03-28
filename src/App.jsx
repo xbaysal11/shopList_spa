8db6c24459d28ed43f62e555c9db52f73b044b04
xbaysal11 <baisalbek.daniiarov@gmail.com>
2019-03-28 20:23:57 +0600

    MODIFIED style

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,12 @@ class App extends Component {
     const { buyItem } = this.state;
     return (
       <div className="App">
-        <div className="title">
+        <div className="center">
           <img src="https://img.icons8.com/clouds/100/000000/online-shop-shopping-bag.png" />
           <h2>Shopping List</h2>
           <form>
-            <input type="text" name="product" placeholder="Add New Item" />
-
-            <input type="submit" value="Submit" />
+            <input type="text" name="product" placeholder="Coffee " />
+            <button type="submit">Add</button>
           </form>
         </div>
         <div className="table-card">
@@ -32,11 +31,16 @@ class App extends Component {
                 <tr className="item-part">
                   <td>1</td>
                   <td>{item}</td>
-                  <td>Button</td>
+                  <td>
+                    <button type="submit">Remove</button>
+                  </td>
                 </tr>
               );
             })}
           </table>
+          <div className="right">
+            <button type="submit">Clear List</button>
+          </div>
         </div>
       </div>
     );
